55116ec094bd02244b9b0dff3a3e503dc78a1d92
Ivan Gurev <ivanzo17898@gmail.com>
2025-10-22 13:37:35 +0000

    added file rendering to frontend

--- ChemCoScientist/streamlit_app.py
+++ ChemCoScientist/streamlit_app.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 
 from ChemCoScientist.frontend import chat, init_page, side_bar
 from ChemCoScientist.frontend.paper_management import paper_management
+from ChemCoScientist.frontend.dataset_management import dataset_management
 from ChemCoScientist.frontend.utils import start_cleanup_thread
+from ChemCoScientist.memory.json_db import JSONFileDB
 from definitions import CONFIG_PATH
 
 load_dotenv(CONFIG_PATH)
@@ -20,9 +22,12 @@ if __name__ == "__main__":
     start_cleanup_thread()
     init_page()
     side_bar()
-    tab_chat, tab_files = st.tabs(["💬 Chat", "📁 File Management"])
+    tab_chat, tab_files, tab_datasets = st.tabs(["💬 Chat", "📁 File Management", "Available Datasets"])
+    db = JSONFileDB(os.environ.get('MEMORY_DB_PATH', 'ChemCoScientist/data_store/files_db.json'))
     with tab_chat:
         chat()
     with tab_files:
         paper_management()
+    with tab_datasets:
+        dataset_management(db)
 
